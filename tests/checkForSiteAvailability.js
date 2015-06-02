c59534fd9cf556a663c7645c8ea48376efb80190
Luka Jerkovic <luka.jerkovic@sistemi.hr>
2015-06-02 16:09:17 +0200

    test

--- tests/checkForSiteAvailability.js
+++ tests/checkForSiteAvailability.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-it('Server should respond to /', function (done) {
+it('Server should respond to / with status ok', function (done) {
   request('http://jerkovicl.github.io/', function (error, response, body) {
     expect(response.statusCode).toBe(200);
     done();
